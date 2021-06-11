03311be32ced13201cd8d1a5b57f09d8e7190d98
Hunter Callaway <HunterCallaway@hotmail.com>
2021-06-11 16:18:05 -0500

    Sliced the array for display purposes.

--- src/NewsSearch.js
+++ src/NewsSearch.js
@@ -67,7 +67,7 @@ const NewsSearch = () => {
   return (
     <Container>
       <Row>
-        {data && data.data.map((article) => (
+        {data && data.data.slice(0, 16).map((article) => (
           <Col xs={12} md={6} lg={3} className="d-flex" key={article.url}>
             <CardDeck className="py-3">
               <Card className="font-card card-background border-secondary">
