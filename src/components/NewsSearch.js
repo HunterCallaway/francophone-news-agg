3527973a80ee49d3b4d685c8900da061571a3ac0
Hunter Callaway <HunterCallaway@hotmail.com>
2022-11-21 17:13:36 -0600

    Added the as prop to Card.Img.

--- src/components/NewsSearch.js
+++ src/components/NewsSearch.js
@@ -8,10 +8,10 @@ import newspapers from '../resources/newspapers.jpeg';
 
 // This function changes the `src` to the default image
 // if there is any error.
-const addErrorSrc = (ev) => {
-  ev.target.src = newspapers; // eslint-disable-line no-param-reassign
-  ev.target.alt = 'Une photo d\'un tas de journaux'; // eslint-disable-line no-param-reassign
-};
+// const addErrorSrc = (ev) => {
+// ev.target.src = newspapers; // eslint-disable-line no-param-reassign
+// ev.target.alt = 'Une photo d\'un tas de journaux'; // eslint-disable-line no-param-reassign
+// };
 
 const NewsSearch = () => {
   // We initialize the state to an empty array. The state will be populated with
@@ -71,9 +71,9 @@ const NewsSearch = () => {
           <Col xs={12} md={6} lg={4} className="d-flex" key={article.url}>
             <CardDeck className="py-3">
               <Card className="font-card card-background border-secondary">
-                {article.image === null || article.image.includes('http') === false
+                {article.image == null || article.image.includes('http') === false
                   ? <Card.Img variant="top" rel="preload" as="image" src={newspapers} alt="Une photo d'un tas de journaux" />
-                  : <Card.Img variant="top" src={article.image} alt="La description de l'image sera accessible en cliquant sur le lien au bas de cette carte." />}
+                  : <Card.Img variant="top" as="image" src={article.image} alt="La description de l'image sera accessible en cliquant sur le lien au bas de cette carte." />}
                 <Card.Body className="d-flex flex-column justify-content-center text-center">
                   <Card.Title className="font-card">
                     {article.title.length > 50
